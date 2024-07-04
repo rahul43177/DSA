77f928c51d76833a70346a78f8f784cad2736347
Rahul Mishra <rahul.mishra@impactanalytics.co>
2024-07-04 23:58:47 +0530

    recursion

---
+++ recursion/ropeCutting.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n =5;
+    int a = 2;
+    int b = 5;
+    int c = 1;
+}
---
+++ recursion/towerOfHanoi.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+void TOH(int n , char a , char b , char c) {
+    if(n == 1) {
+        cout << "Move 1 from " << a << " to " << c << endl;
+        return;
+    }
+    TOH(n-1 , a , c , b);
+    cout << "Move " << n << " from " << a << " to " << c << endl;
+    TOH(n-1 , b , a , c);
+    
+}
+int main() {
+    int n =2;
+    char  a = 'A';
+    char b = 'B';
+    char c = 'C';
+    TOH(n,a,b,c);
+}
